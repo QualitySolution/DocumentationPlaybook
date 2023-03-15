3a63729bafa21a2cc83d42be306418263296d6c7
Gankov <gav@qsolution.ru>
2023-03-15 20:14:46 +0300

    Доработал скрипт локального запуска
    Возможность запуска из любой папки.
    Задержка при открытии браузера.

--- build-local.sh
+++ build-local.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")" #Изменяем текущую дерикторию, на ту в которой лежит скрипт, иначе при вызове скрипта из другой дериктории пути будут неправильными.
 antora generate antora-playbook-author-mode.yml
 INDEX_FILE=$(realpath "build/site/index.html")
 echo ${INDEX_FILE}
 xdg-open file://${INDEX_FILE}
+
+sleep 2 #Без задержки браузер почему-то не окрывал страницу.
